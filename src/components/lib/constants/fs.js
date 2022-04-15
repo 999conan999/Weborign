bfe294e5a1ebf0f7b365e3b68d51675e7e4160cb
vothanhdanh <999conan999@gmail.com>
2022-04-16 00:20:38 +0700

    v1.09090899999

--- src/components/lib/constants/fs.js
+++ src/components/lib/constants/fs.js
@@ -1,7 +1,11 @@
 export function fs_convert_schema_cript(arr){
     let script=''
     arr.forEach(e => {
-        script+='<script type="application/ld+json">'+e+'</script>';
+        if(e.search('<script')>-1){
+            script+=e;
+        }else{
+            script+='<script type="application/ld+json">'+e+'</script>';
+        }
     });
     return script;
 }
